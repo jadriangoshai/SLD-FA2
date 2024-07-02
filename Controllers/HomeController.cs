af32dcfd414c5d8712c16a5816d810e5134ad8a5
jadriangoshai <88934158+jadriangoshai@users.noreply.github.com>
2024-07-03 01:59:50 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrialRun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {            
+            return View();
+        }
+        //Favourites webpage added
+        public ActionResult Favourites()
+        {
+            return View();
+        }
+
+        //Location added from Layout
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Location of where you can find us.";
+
+            return View();
+        }
+        //News added
+        public ActionResult News()
+        {
+            return Redirect("https://readersden.co.za/category/news-general/");           
+        }
+    }
+}
